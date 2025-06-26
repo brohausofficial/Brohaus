679d316bce3a15326dfd08d1ac2a65feb1d15924
priyanshu anand <priyanshuanand@omfysgroup.com>
2025-06-26 23:12:53 +0530

    add product update

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+ import React, { useState } from 'react'
 import {assets} from '../assets/assets'
 import axios from 'axios'
 import { backendUrl } from '../App'
@@ -39,7 +39,7 @@ const Add = ({token}) => {
       image3 && formData.append("image3",image3)
       image4 && formData.append("image4",image4)
 
-      const response = await axios.post(backendUrl + "/api/product/add",formData,{headers:{token}})
+      const response = await axios.post(backendUrl + "/api/product/add",formData,{headers:{Authorization: `Bearer ${token}`}})
 
       if (response.data.success) {
         toast.success(response.data.message)
